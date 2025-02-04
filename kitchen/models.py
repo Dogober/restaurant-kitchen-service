1388394df17de5472f25869ad4b2878f1e58b16c
git_practic <dogoberrr@gmail.com>
2025-02-05 01:14:59 +0200

    add description text field to the DishType model

--- kitchen/models.py
+++ kitchen/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
         ordering = ["name"]
