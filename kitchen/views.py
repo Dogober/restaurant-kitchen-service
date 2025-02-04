c076218672fcb9992a1fb3f35db65786e8ced42e
git_practic <dogoberrr@gmail.com>
2025-02-04 11:50:04 +0200

    add index_view, index_html and appropriate url with test data to ensure that all works as expected

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_users = get_user_model().objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_users": num_users,
+        "num_dishes": num_dishes,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
