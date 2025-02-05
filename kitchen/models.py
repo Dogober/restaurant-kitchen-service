0bd0864d4bebe8faeef6e2121e93127347ef741e
git_practic <dogoberrr@gmail.com>
2025-02-05 02:48:39 +0200

    add description field for Category model and implement it's str method

--- kitchen/models.py
+++ kitchen/models.py
@@ -26,9 +26,13 @@ class User(AbstractUser):
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
         ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Ingredient(models.Model):
